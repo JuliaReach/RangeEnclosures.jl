d02a425f23d91f23aa386c04d8b65edbc7241a12
Marcelo Forets <mforets@gmail.com>
2019-08-10 09:21:14 +0200

    update branch and bound (#13)
    * update branch and bound
    
    * fix Rnext

--- src/branchandbound.jl
+++ src/branchandbound.jl
@@ -1,64 +1,56 @@
-@inline _Rnext(R::Array{Interval{T}, 1}) where{T} = Interval(minimum(inf.(R)),
-                                                             maximum(sup.(R)))
 # univariate
-function enclose_BranchandBound(f::Function, dom::Interval{T}; order::Int = 10,
-                                                     tol::Number = 0.6) where{T}
+function enclose_BranchandBound(f::Function, dom::Interval; order=10, tol=0.6) 
     x0 = Interval(mid(dom))
     x = TaylorModel1(order, x0, dom)
     return branchandbound(f(x-x0).pol, dom, tol)
 end
+
 # multivariate
-function enclose_BranchandBound(f::Function, dom::IntervalBox{T, N};
-                                order::Int = 10, tol::Number = 0.6) where {T, N}
-    n = length(dom)
+function enclose_BranchandBound(f::Function, dom::IntervalBox{D};
+                                order=10, tol=0.6) where {D}
     x0 = mid(dom)
-    set_variables(Float64, "x", order=2order, numvars=n)
-    x = [TaylorModelN(i, order, IntervalBox(x0), dom) for i=1:n]
+    set_variables(Float64, "x", order=2order, numvars=D)
+    x = [TaylorModelN(i, order, IntervalBox(x0), dom) for i=1:D]
     return branchandbound(f(x...).pol, dom - x0, tol)
 end
 
-function branchandbound(p::Taylor1{T}, dom::Interval{W}, ϵ::Number) where{T, W}
-    return _branchandbound(p, dom, ϵ)
-end
+@inline _Rnext(R::Vector{<:Interval}) = Interval(minimum(inf.(R)), maximum(sup.(R)))
 
-function branchandbound(p::TaylorN{T}, dom::IntervalBox{N, W},
-                                       ϵ::Number) where{T, N, W}
-    return _branchandbound(p, dom, ϵ)
-end
+const Kmax = 1000
 
-function _branchandbound(p::Union{TaylorN{T}, Taylor1{T}},
-                         dom::Union{IntervalBox{N, W}, Interval{W}},
-                         ϵ::Number) where {N, T, W}
+function branchandbound(p::Union{Taylor1, TaylorN},
+                        dom::Interval_or_IntervalBox,
+                        tol::Number)
     K = 1
-    Rprev = evaluate(p, dom) #Rprevious
-    D1, D2 = bisect(dom)
-    D = [D1, D2]
-    R = [evaluate(p, D[i]) for i = 1:length(D)]
-    Rnext = _Rnext(R)
-    while  (Rprev.hi - Rnext.hi) >= ϵ*diam(Rnext) &&
-           (Rprev.lo - Rnext.lo) >= ϵ*diam(Rnext) && (K <= 1000)
+    Rprev = evaluate(p, dom)
+    D = bisect(dom)
+    R = [evaluate(p, D[1]), evaluate(p, D[2])]
+    Rnext = R[1] ∩ R[2]
+
+    while  (Rprev.hi - Rnext.hi) >= tol * diam(Rnext) &&
+           (Rprev.lo - Rnext.lo) >= tol * diam(Rnext)
+
         Rprev = _Rnext(R)
-        R_x = [R[i].hi for i = 1:length(R)]
-        R_n = [R[i].lo for i = 1:length(R)]
-        max_range = maximum(R[i].hi for i = 1:length(R))
+        R_x = sup.(R)
+        R_n = inf.(R)
+        max_range = maximum(R_x)
         max_index = findall(x->x == max_range, R_x)[1]
-        min_range = minimum(R[i].lo for i = 1:length(R))
+        min_range = minimum(R_n)
         min_index = findall(x->x == min_range, R_n)[1]
         l_D = length(D[1])
-        K = K + 1
-        if min_index == max_index
-            D, R = divide_dom!(p, D, R, max_index)
-            Rnext = _Rnext(R)
-        else
-            D, R = divide_dom!(p, D, R, max_index)
-            if max_index < min_index
-                min_index = min_index + 1
-            end
-            D, R = divide_dom!(p, D, R, min_index)
-            Rnext = _Rnext(R)
+        K += 1
+        if K == Kmax
+            error("convergence not achieved in branch and bound after $Kmax steps")
         end
+
+        D, R = divide_dom!(p, D, R, max_index)
+        if max_index < min_index
+            min_index += 1
+            D, R = divide_dom!(p, D, R, min_index)
+        end
+        Rnext = _Rnext(R)
     end
-    return  _Rnext(R)
+    return _Rnext(R)
 end
 
 function divide_dom!(p::Union{TaylorN{T}, Taylor1{T}},
