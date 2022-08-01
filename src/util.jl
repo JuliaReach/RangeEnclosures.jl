4305376f79e97bb1525c228b71ce0f87233e93a8
schillic <git@christianschilling.net>
2022-08-01 14:59:33 +0200

    add ReachabilityBase dependency

--- src/util.jl
+++
@@ -1,7 +0,0 @@
-function require(package::Symbol; fun_name::String="", explanation::String="")
-    check = isdefined(@__MODULE__, package)
-    @assert check "package '$package' not loaded" *
-        (fun_name == "" ? "" :
-            " (it is required for executing `$fun_name`" *
-            (explanation == "" ? "" : " " * explanation) * ")")
-end
