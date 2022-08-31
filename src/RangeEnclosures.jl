40c1c21713720ea75b3fbe2fc6178253a3a2c0a6
schillic <git@christianschilling.net>
2022-08-31 22:12:31 +0200

    add require for AffineArithmetic

--- src/RangeEnclosures.jl
+++ src/RangeEnclosures.jl
@@ -12,6 +12,7 @@ include("algorithms.jl")
 include("intervalarithmetic.jl")
 include("branchandbound.jl")
 include("taylormodels.jl")
+include("affine.jl")
 include("sdp_header.jl")
 include("intervaloptimisation.jl")
 
@@ -20,7 +21,7 @@ include("intervaloptimisation.jl")
 # ================
 
 function __init__()
-    @require AffineArithmetic = "2e89c364-fad6-56cb-99bd-ebadcd2cf8d2" include("affine.jl")
+    @require AffineArithmetic = "2e89c364-fad6-56cb-99bd-ebadcd2cf8d2" eval(load_affinearithmetic())
     @require SumOfSquares = "4b9e565b-77fc-50a5-a571-1244f986bda1" include("sdp.jl")
     @require TaylorModels = "314ce334-5f6e-57ae-acf6-00b6e903104a" eval(load_taylormodels())
     @require IntervalOptimisation = "c7c68f13-a4a2-5b9a-b424-07d005f8d9d2" eval(load_intervaloptimization())
