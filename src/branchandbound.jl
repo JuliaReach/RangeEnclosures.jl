6a9b1277fbdda6f5bdb2bf6940d345a2363a787d
schillic <git@christianschilling.net>
2022-10-01 10:23:38 +0200

    test BranchAndBoundEnclosure with non-monotonic function

--- src/branchandbound.jl
+++ src/branchandbound.jl
@@ -44,7 +44,7 @@ function _monotonicity_check(f::Function, X::IntervalBox{N}, ∇fX::AbstractVect
     high = zeros(eltype(X), N)
 
     @inbounds for (i, di) in enumerate(∇fX)
-        if di >=0  #  increasing
+        if di >= 0  #  increasing
             high[i] = sup(X[i])
             low[i] = inf(X[i])
         elseif di <= 0  # decreasing
