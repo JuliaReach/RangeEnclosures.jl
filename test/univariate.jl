2160907cfbbe79438e3eacce4310304ee125e9c7
Christian Schilling <git@christianschilling.net>
2022-10-07 15:31:55 +0200

    Test MeanValueEnclosure (#107)
    * test MeanValueEnclosure
    
    * remove old test code
    
    * test results for each solver individually

--- test/univariate.jl
+++ test/univariate.jl
@@ -7,19 +7,20 @@
     for solver in available_solvers
         x = enclose(f, dom, solver)
         rleft, rright = relative_precision(x, xref)
-        if solver isa TaylorModelsEnclosure || solver isa AffineArithmeticEnclosure
+        if solver isa TaylorModelsEnclosure
             # for this example, TaylorModels cannot tightly approximate the lhs
             @test rleft ≤ 30 && rright ≤ 1e-10
-        elseif solver isa SumOfSquaresEnclosure
-            # solver returns negative -1.06259e-06
-            @test rleft ≥ -1e-5 && rright ≤ 1e-5
+        elseif solver isa AffineArithmeticEnclosure
+            @test rleft ≤ 22 && rright ≤ 1e-10
+        elseif solver isa MeanValueEnclosure
+            @test rleft ≤ 94 && rright ≤ 39
         else
             @test rleft ≤ 1e-5 && rright ≤ 1e-5
         end
     end
 end
 
-@testset "Univariate example the Quickstart Guide" begin
+@testset "Univariate example from the Quickstart Guide" begin
     f(x) = -x^3/6 + 5x
     dom = Interval(1, 4)
 
