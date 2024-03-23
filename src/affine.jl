fabc4e524ec29e9e212b19181d1c8679df3ee695
schillic <git@christianschilling.net>
2024-03-23 23:33:43 +0100

    vector output

--- src/affine.jl
+++ src/affine.jl
@@ -13,7 +13,7 @@ function enclose(f::Function, dom::Interval, ::AffineArithmeticEnclosure)
     require(@__MODULE__, :AffineArithmetic; fun_name="enclose")
 
     x = Aff(dom, 1, 1)
-    return interval(f(x))
+    return _wrap_output(f(x))
 end
 
 # multivariate
@@ -21,5 +21,5 @@ function enclose(f::Function, dom::IntervalBox{N}, ::AffineArithmeticEnclosure) 
     require(@__MODULE__, :AffineArithmetic; fun_name="enclose")
 
     x = [Aff(dom[i], N, i) for i in 1:N]
-    return interval(f(x))
+    return _wrap_output(f(x))
 end
