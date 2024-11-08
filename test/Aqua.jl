ec683a41607023478b1af52d07550ef681b5e205
schillic <git@christianschilling.net>
2024-11-08 22:05:47 +0100

    update Aqua tests

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -2,5 +2,10 @@ using RangeEnclosures, Test
 import Aqua
 
 @testset "Aqua tests" begin
-    Aqua.test_all(RangeEnclosures)
+    if VERSION >= v"1.7"
+        Aqua.test_all(RangeEnclosures)
+    else
+        # some tests fail in v1.6 due to problems in SDPA
+        Aqua.test_all(RangeEnclosures; stale_deps=false, deps_compat=false)
+    end
 end
