29e9c5b64a115abebd41f70a9ea532562c34fbc1
schillic <git@christianschilling.net>
2024-11-16 11:34:34 +0100

    undo SDPA workaround

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -2,10 +2,5 @@ using RangeEnclosures, Test
 import Aqua
 
 @testset "Aqua tests" begin
-    if VERSION >= v"1.7"
-        Aqua.test_all(RangeEnclosures)
-    else
-        # some tests fail in v1.6 due to problems in SDPA
-        Aqua.test_all(RangeEnclosures; stale_deps=false, deps_compat=false)
-    end
+    Aqua.test_all(RangeEnclosures)
 end
